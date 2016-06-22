893edc40d18dfeb09c53b9d21b0be136c5cb0a08
Kevin McGregor <kevin.mcgregor@mail.mcgill.ca>
2016-06-22 11:47:15 -0400

    Added RDS files... also added R file to try clustering based on PCA

---
+++ R/cluster_by_pca.R
@@ -0,0 +1,7 @@
+# Trying PCA to see if we can identify clusters based on litter
+
+mouse.data <- readRDS("~/Documents/research/causal_challenge_repo/data/mouse_data.RDS")
+var.names <- readRDS("~/Documents/research/causal_challenge_repo/data/variable_names.RDS")
+
+
+
