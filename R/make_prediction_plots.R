013b859e1b8b9cbab68cb4a568e833a17a795347
Kevin McGregor <kevin.mcgregor@mail.mcgill.ca>
2016-07-24 16:32:39 -0400

    Changes to poster (new figures, changed configuration in results)

--- R/make_prediction_plots.R
+++ R/make_prediction_plots.R
@@ -1,9 +1,9 @@
 #Make nice plots for poster (prediction stuff)
 library(ggplot2)
 
-load("~/Documents/research/causal_challenge_repo/data/simulation_final.RData")
+load("~/Documents/research/causal_challenge_repo/data/simulation_more_final.RData")
 
-plot_data = data.frame(c(res_replace29[,1],res_replace29[,2],res_replace29[,3],res_replace29[,4],res_replace29[,5]),
+plot_data = data.frame(log(c(res_direct[,1],res_direct[,2],res_direct[,3],res_direct[,4],res_direct[,5])),
                        rep(1:5,each = 100))
 colnames(plot_data) = c("MSE", "Variable")
 
@@ -15,13 +15,13 @@ gplot+geom_boxplot(fill="darkgreen", color="red") +
                             "Lymph diff count",
                             "Mono cell count",
                             "Baso diff count")) + theme_bw() +
-  theme(axis.text.x=element_text(angle=25,size=11,hjust = 1),axis.title.x=element_blank(),
-        axis.title.y=element_text(size=14)) +
+  theme(axis.text.x=element_text(angle=25,size=11,hjust = 1),
+        axis.title.y=element_text(size=14), axis.title.x=element_blank()) + ylab("log(MSE)") +
   ggtitle("Mean square error of simulation predictions")
 dev.off()
 
 #Plotting final imputed values for var 29 among the true values in the other genotypes
-load("~/Documents/research/causal_challenge_repo/Results/FINAL_PREDICTIONS.RData")
+load("~/Documents/research/causal_challenge_repo/Results/MORE_FINAL_PREDICTIONS.RData")
 
 pdf("~/Documents/research/causal_challenge_repo/plots/var29_plot.pdf",width=9,height=5)
 gplot2 = ggplot(mouse.data.imp, aes(factor(geno), IMPC_HEM_029_001))
